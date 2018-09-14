ff920b73d379f3042fe66eafb52311a65a822511
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2018-09-13 22:10:25 -0500

    Se crearon Queryset basicas

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
+
 def post_create(request):
     return HttpResponse("Create")
 
@@ -11,7 +13,9 @@ def post_detail(request):
     return render(request, "index.html", context)
 
 def post_list(request):
+    queryset = Post.objects.all()
     context = {
+        "object_list": queryset,
         "title": "List"
     }
     
