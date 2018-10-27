48a5695ddb8e88cf003bc2c0555371f78637c2eb
Luis Fernando Nieves Nieves <luis.fernan.nieves@gmail.com>
2018-10-26 21:30:51 -0500

    Post with user, foreignkey

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,19 @@
 from urllib import quote_plus
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
+    # if not request.user.is_authenticated():
+    #    raise Http404
+
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
